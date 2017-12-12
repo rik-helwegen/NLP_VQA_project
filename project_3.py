eaabc5429d1aa98090d392f6949eac8178a7cc0f
Sierk <sierkkanis@hotmail.com>
2017-12-12 16:26:22 +0100

    fix test_loss bug + weight decay

--- project_3.py
+++ project_3.py
@@ -79,7 +79,7 @@ def one_hot_encoding(data):
 # to speed-up training, max = len(x_train)
 # training_size = len(x_train)
 
-training_size = 3000
+training_size = 1000
 # test_size = len(x_test)
 test_size = 100
 # validation test_size
@@ -105,7 +105,7 @@ questions_test = [x[1] for x in x_test]
 questions_test = one_hot_encoding(questions_test)
 
 # test
-answers_validation = y_validation[]
+answers_validation = y_validation
 img_ids_validation = [x[0] for x in x_validation]
 questions_validation = [x[1] for x in x_validation]
 # encodes to one hot vector
@@ -129,7 +129,6 @@ class CBOW(nn.Module):
         self.embedding = nn.Linear(vocab_size, embedding_dim)
         self.embedding_output = nn.Linear(embedding_dim, output_dim)
         self.img_output = nn.Linear(image_features_dim, output_dim)
-        self.softmax = nn.Softmax()
 
     def forward(self, question_input, image_input):
         embeds = self.embedding(question_input)
@@ -137,47 +136,46 @@ class CBOW(nn.Module):
         # img_output = self.img_output(image_input)
         # addition = torch.add(embedding_output, img_output)
 
-        return self.softmax(embedding_output)
+        return embedding_output
 
 
-model = CBOW(nwords, 2000, nfeatures, ntags)
+model = CBOW(nwords, 164, nfeatures, ntags)
 
 print(model)
 
 
 def evaluate(model, test_data):
     """Evaluate a model on a data set."""
-    eval_loss = 0.0
+    test_loss = 0.0
     correct = 0.0
-
+    np.random.shuffle(test_data)
     # forward with batch size = 1
     for i in range(len(test_data)):
-
         question = test_data[i][0]
         answer = test_data[i][1]
         img_id = test_data[i][2]
         image = np.ndarray.tolist(img_features[visual_feat_mapping[str(img_id)]])
 
-        eval_question_tensor = Variable(torch.FloatTensor([question]))
-        eval_image_tensor = Variable(torch.FloatTensor([image]))
-
-        eval_scores = model(eval_question_tensor, eval_image_tensor)
-        eval_target = Variable(torch.LongTensor([answer]))
-
+        question_tensor = Variable(torch.FloatTensor([question]))
+        image_features_tensor = Variable(torch.FloatTensor([image]))
+        scores = model(question_tensor, image_features_tensor)
         loss = nn.CrossEntropyLoss()
+        target = Variable(torch.LongTensor([answer]))
 
-        eval_output = loss(eval_scores, eval_target)
-        eval_loss += eval_output.data[0]
+        output = loss(scores, target)
+        test_loss += output.data[0]
 
         # measure accuracy of prediction
-        predict = eval_scores.data.numpy().argmax(axis=1)[0]
+        predict = scores.data.numpy().argmax(axis=1)[0]
         if predict == answer:
             correct += 1
 
-    return correct/len(test_data) * 100, eval_loss/len(test_data)
+    accuracy = correct / len(test_data) * 100
+    avg_test_loss = test_loss/len(test_data)
+    return accuracy, avg_test_loss
 
 
-optimizer = optim.Adam(model.parameters(), lr=0.0001)
+optimizer = optim.Adam(model.parameters(), lr=0.01, weight_decay=0.005)
 # different layers must use different learning rates
 # optimizer = optim.Adam([
 #     {'params': model.embedding.parameters(), 'lr': 1e-1}
@@ -193,8 +191,8 @@ minibatch_size = 35
 
 # Number of epochs
 
-epochs = 50
-# after which number of epochs we want a evaluation:
+epochs = 15
+# after which number of epochs we want an evaluation:
 test_update = 1
 # create zero vectors to save progress
 learning_train = np.zeros([epochs, 2])
@@ -203,9 +201,10 @@ learning_test = np.zeros([int(math.floor((epochs-1)/test_update))+1, 3])
 for ITER in range(epochs):
     train_loss = 0.0
     start = time.time()
-
+    batches_count = 0
     # split up data in mini-batches
     for i in range(0, training_data.shape[0], minibatch_size):
+        batches_count += 1
         np.random.shuffle(training_data)
         batch = training_data[i:i + minibatch_size]
         input_questions = [x[0] for x in batch]
@@ -232,16 +231,45 @@ for ITER in range(epochs):
         # update weights
         optimizer.step()
 
+    # training progress
     print("iter %r: train loss/sent %.6f, time=%.2fs" %
-          (ITER, train_loss/len(training_data), time.time() - start))
+          (ITER, train_loss/batches_count, time.time() - start))
+    learning_train[ITER, :] = [ITER, train_loss/batches_count]
 
-    # evaluate, only evaluate every 'test_update' iterations, to save time
-    if ITER % test_update == 0:
-        acc, avg_eval_loss = evaluate(model, test_data)
-        print("iter %r: test loss/sent %.6f, accuracy=%.6f" % (ITER, avg_eval_loss, acc))
-        learning_test[int(ITER/test_update), :] = [ITER, avg_eval_loss, acc]
+    # testing progress
+    acc, avg_test_loss = evaluate(model, training_data)
+    print("iter %r: test loss/sent %.6f, accuracy=%.6f" % (ITER, avg_test_loss, acc))
+    learning_test[ITER, :] = [ITER, avg_test_loss, acc]
 
-    learning_train[ITER, :] = [ITER, train_loss/len(training_data)]
+    # # evaluate, only evaluate every 'test_update' iterations, to save time
+    # if ITER % test_update == 0:
+    #     acc, avg_test_loss = evaluate(model, training_data)
+    #     print("iter %r: test loss/sent %.6f, accuracy=%.6f" % (ITER, avg_test_loss, acc))
+    #     learning_test[int(ITER/test_update), :] = [ITER, avg_test_loss, acc]
+
+#final eval
+# np.random.shuffle(test_data)
+# test_data = test_data[:30]
+np.random.shuffle(training_data)
+test_data = training_data[:30]
+predictions = []
+for i in range(len(test_data)):
+    question = test_data[i][0]
+    answer = test_data[i][1]
+    img_id = test_data[i][2]
+    image = np.ndarray.tolist(img_features[visual_feat_mapping[str(img_id)]])
+
+    eval_question_tensor = Variable(torch.FloatTensor([question]))
+    eval_image_tensor = Variable(torch.FloatTensor([image]))
+
+    eval_scores = model(eval_question_tensor, eval_image_tensor)
+    eval_target = Variable(torch.LongTensor([answer]))
+    predict = eval_scores.data.numpy().argmax(axis=1)[0]
+    # if predict == answer:
+    #     correct += 1
+    predictions.append(predict)
+
+print("predictions =", predictions)
 
 
 plt.close('all')
